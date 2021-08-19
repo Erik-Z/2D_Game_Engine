bac764b214acd044150ba2a96a112cc5c98e7aea
Erik Z <erikzhou7@gmail.com>
2021-08-19 01:05:44 -0400

    Added Direction Constants

---
+++ src/main/kotlin/baseAPIs/Direction.kt
@@ -0,0 +1,11 @@
+package baseAPIs
+
+object DirConsts {
+    const val DIR_FRONT: Int = 0
+    const val DIR_Bottom: Int = 0
+    const val DIR_BACK: Int = 1
+    const val DIR_TOP: Int = 1
+    const val DIR_LEFT: Int = 2
+    const val DIR_RIGHT: Int = 3
+    const val DIR_NONE: Int = -1
+}
