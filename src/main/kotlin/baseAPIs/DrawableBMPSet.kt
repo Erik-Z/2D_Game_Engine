7485277f8bfa2674c77aa958810bef585f8f244d
Erik Z <erikzhou7@gmail.com>
2021-08-24 20:16:59 -0400

    Added BMP Drawable set class

---
+++ src/main/kotlin/baseAPIs/DrawableBMPSet.kt
@@ -0,0 +1,14 @@
+package baseAPIs
+
+import java.awt.Graphics2D
+import java.awt.image.BufferedImage
+
+class DrawableBMPSet {
+    var buffImg: BufferedImage? = null
+
+    var graphics: Graphics2D? = null
+
+    var p: Pen? = null
+
+    var img: BMPImage? = null
+}
